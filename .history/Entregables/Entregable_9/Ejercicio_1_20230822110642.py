427d16507b9c4827da3d35dd3640a6cd2054ee53
mauricioos <mauricioos@gmail.com>
2023-08-22 11:31:31 -0600

    Termine

---
+++ .history/Entregables/Entregable_9/Ejercicio_1_20230822110642.py
@@ -0,0 +1,15 @@
+import random as r
+from functools import reduce
+list_multplicaciones = [(r.randint(1,10),r.randint(1,10)) for i in range(10)]
+def juego_multiplicar(cont:int, multiplicacion:tuple)->int:
+    resultado = (Multi[0])*(multi[1])
+    respuesta = int(input(f"{Multi[0]}*{multi[1]} "))
+    if respuesta == resultado:
+        cont+=1
+        return cont
+    else:
+        print(f"Error la respuesta correcta es = {resultado}")
+        return cont
+        puntos = reduce(juego_multiplicar,list_multplicaciones,0)
+
+
---
+++ .history/Entregables/Entregable_9/Ejercicio_1_20230822112601.py
@@ -0,0 +1,20 @@
+import random as r
+from functools import reduce
+
+listmultplicaciones = [(r.randint(1,10),r.randint(1,10)) for i in range(10)]
+
+def juego_multiplicar(cont:int, mult:tuple)->int:
+    resultado = mult[0] * mult[1]
+    respuesta = int(input(f"{mult[0]} * {mult[1]} "))
+    if respuesta == resultado:
+        cont+=1
+        return cont
+    else:
+        print(f"Error la respuesta correcta es = {resultado}")
+        return cont
+
+puntos = reduce(juego_multiplicar,listmultplicaciones,0)    
+print(f"tu puntuacion es {puntos}")
+
+
+
